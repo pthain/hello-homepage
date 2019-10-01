20d9a7510f8e34a6bd639f3b9d82de377d2610cc
Phillip Thain <pthain@purdue.edu>
2019-09-30 22:52:30 -0400

    Use a function to display the time using React

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 function App() {
   //Outline for the homepage
+
   return (
     <div>
-
       <h1>Welcome.</h1>
       <img src={logo} className="App-logo" alt="logo"/>
       <div id='navBar'>
@@ -14,9 +14,15 @@ function App() {
         <p id='resume'>Resume</p>
         <p id='portfolio'>Portfolio</p>
       </div>
-      <div id='display'>Dynamic content will be loaded here depending on which page the user chooses.</div>
+      <div id='display'>{setDisplayDiv()}</div>
     </div>
   )
+
+function setDisplayDiv() {
+  return (
+    Date.now().toLocaleString()
+  )
+}
   /*
   return (
     <div className="App">
