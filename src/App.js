3e2e23baf13a65d6c69f756d46fa5db52ecd339e
Phillip Thain <pthain@purdue.edu>
2019-09-30 23:41:19 -0400

    Use react to update state based off of user input

--- src/App.js
+++ src/App.js
@@ -2,26 +2,71 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  //Outline for the homepage
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      choice: 0
+    }
+  }
 
+  handleClick(i) {
+    this.setState({
+      choice: i
+    })
+  }
+
+  //Outline for the homepage
+render() {
   return (
     <div>
       <h1>Welcome.</h1>
       <img src={logo} className="App-logo" alt="logo"/>
       <div id='navBar'>
-        <p id='home'>Home</p>
-        <p id='resume'>Resume</p>
-        <p id='portfolio'>Portfolio</p>
+        <p
+          id='home'
+          onClick={() => this.handleClick(0)}>Home
+        </p>
+        <p id='resume' onClick={() => this.handleClick(1)}>Resume</p>
+        <p id='portfolio' onClick={() => this.handleClick(2)}>Portfolio</p>
       </div>
-      <div id='display'>{setDisplayDiv()}</div>
+      <div id='display'>{setDisplayDiv(this.state.choice)}</div>
     </div>
   )
 
-function setDisplayDiv() {
+/*function setDisplayDiv() {
   return (
-    Date.now().toLocaleString()
+    //Date.now().toLocaleString()
+    //Return a REACT ELEMENT
+    <div
+      id="htmlContent"
+      innerHtml='<object type="text/html" data="../public/html/home.html" ></object>'
+    >
+    </div>
   )
+}*/
+function setDisplayDiv(i) {
+  if (i === 0) {
+    return (
+      <div>
+        <h1>HOME!</h1>
+      </div>
+    )
+  }
+  if (i === 1) {
+    return (
+      <div>
+        <h1>RESUME!</h1>
+      </div>
+    )
+  }
+  if (i === 2) {
+    return (
+      <div>
+        <h1>PORTFOLIO!</h1>
+      </div>
+    )
+  }
 }
   /*
   return (
@@ -44,5 +89,5 @@ function setDisplayDiv() {
   );
   */
 }
-
+}
 export default App;
